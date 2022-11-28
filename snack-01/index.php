d491b1a47c49287dc341f246a1a3eb8c2a610e49
Francesca-Barcella <pollon.fra@gmail>
2022-11-28 12:48:53 +0100

    snack-02 initial commit

--- snack-01/index.php
+++ snack-01/index.php
@@ -2,4 +2,25 @@
 /* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 */
 
+$array_matches [
+    [
+        'squadra_casa' => 'olimpia Milano',
+'squadra_ospiti' => 'Cantu',
+        'casa_punti' => 10,
+        'casa_punti' => 20
+    ],
+    [
+ 'squadra_casa' => 'Bergamo',
+        'squadra_ospiti' => 'Monza',
+        'casa_punti' => 5,
+        'casa_punti' => 15
+    ],
+    [
+        'squadra_casa' => 'olimpia Milano',
+        'squadra_ospiti' => 'Cantu',
+        'casa_punti' => 10,
+        'casa_punti' => 20
+    ]
+]
+
 ?>
