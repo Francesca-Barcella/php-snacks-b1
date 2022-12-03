54a68a5ce4f45e0db860e37533df3a579e0f4954
Francesca-Barcella <pollon.fra@gmail>
2022-12-03 22:29:40 +0100

    fix bug

--- snack-02/index.php
+++ snack-02/index.php
@@ -16,7 +16,21 @@
 </head>
 
 <body>
-    <form action="index.php" method="GET"></form>
+    <form action="index.php" method="GET">
+        <div>
+            <label for="nome">Nome</label>
+            <input name="nome" id="nome" type="text">
+        </div>
+        <div>
+            <label for="Mail">Mail</label>
+            <input name="Mail" id="Mail" type="text">
+        </div>
+        <div>
+            <label for="age">Age</label>
+            <input name="age" id="age" type="text">
+        </div>
+
+    </form>
 
 </body>
 
